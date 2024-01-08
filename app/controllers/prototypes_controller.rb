a9e8a0665b456858977653197117a36d9b241b9b
kin0208 <kimsangsu110208@gmail.com>
2024-01-08 18:21:00 +0900

    createアクションとストロングパラメーターなど

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,5 +7,15 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    if current_user.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
