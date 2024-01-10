dc0ed5a1eaff9ca31a44b4b9b379ee351869dc19
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-10 12:26:26 +0900

    user詳細ページでuser情報の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    # Pathパラメータで送信されるID値で、Userモデルの特定のオブジェクトを取得するように記述し、それを@userに代入した
   end
 
   private
