bd10e5b4ed3abbfb67bcf276c3f4806a86d3daaa
Ch1HIrO3 <ydddw584@yahoo.co.jp>
2024-01-10 18:20:14 +0900

    before Action宛先変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_prototype, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
-
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.includes(:user)
